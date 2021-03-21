0e4ec44110e965a7c0689e86413fffdc83f8419a
Prophet731 <Prophet731@users.noreply.github.com>
2021-03-20 18:14:42 -0700

    Make Config file compatable with BFACP 2.1-dev

--- config/config.php
+++ config/config.php
@@ -1,4 +1,14 @@
 <?php
+
+if (file_exists(__DIR__ . '/../../../../vendor/autoload.php')) {
+    require __DIR__ . '/../../../../vendor/autoload.php';
+
+    $dotenv = new \Dotenv\Dotenv(__DIR__ . '/../../../../');
+    $dotenv->load();
+} else {
+    die('Failed to load');
+}
+
 // BF4 Stats Page by Ty_ger07
 // https://forum.myrcon.com/showthread.php?6854
 
@@ -6,18 +16,17 @@
 // Read readme.txt for help.
 
 // DATABASE INFORMATION
-DEFINE('HOST', '');													// database host address
+DEFINE('HOST', $_SERVER['DB_HOST']);													// database host address
 DEFINE('PORT', '3306');												// database port - default is 3306
-DEFINE('NAME', '');													// database name
-DEFINE('USER', '');													// database user name - sometimes the same as the database name
-DEFINE('PASS', '');													// database password
+DEFINE('NAME', $_SERVER['DB_DATABASE']);													// database name
+DEFINE('USER', $_SERVER['DB_USERNAME']);													// database user name - sometimes the same as the database name
+DEFINE('PASS', $_SERVER['DB_PASSWORD']);													// database password
 
 // CLAN NAME
-$clan_name		= 'clan';											// your gaming clan or organization name
+$clan_name		= $_SERVER['APP_NAME'];											// your gaming clan or organization name
 
 // PAGE BANNER
 $banner_image	= './common/images/bf4-logo.png';					// your desired page banner image
 
 // BANNER LINK
-$banner_url		= 'http://tyger07.github.io/BF4-Server-Stats/';		// where clicking on page banner will take you
-?>
+$banner_url		= $_SERVER['APP_URL'];		// where clicking on page banner will take you
