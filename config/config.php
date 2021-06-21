0fef36ff9278b7e9d692987ad184f21f1b066c16
Prophet731 <Prophet731@users.noreply.github.com>
2021-06-21 09:39:30 -0700

    Added port

--- config/config.php
+++ config/config.php
@@ -16,17 +16,17 @@ if (file_exists(__DIR__ . '/../../../../vendor/autoload.php')) {
 // Read readme.txt for help.
 
 // DATABASE INFORMATION
-DEFINE('HOST', $_SERVER['DB_HOST']);													// database host address
-DEFINE('PORT', '3306');												// database port - default is 3306
-DEFINE('NAME', $_SERVER['DB_DATABASE']);													// database name
-DEFINE('USER', $_SERVER['DB_USERNAME']);													// database user name - sometimes the same as the database name
-DEFINE('PASS', $_SERVER['DB_PASSWORD']);													// database password
+DEFINE('HOST', $_SERVER['DB_HOST']);                                                    // database host address
+DEFINE('PORT', $_SERVER['DB_PORT']);                                                // database port - default is 3306
+DEFINE('NAME', $_SERVER['DB_DATABASE']);                                                    // database name
+DEFINE('USER', $_SERVER['DB_USERNAME']);                                                    // database user name - sometimes the same as the database name
+DEFINE('PASS', $_SERVER['DB_PASSWORD']);                                                    // database password
 
 // CLAN NAME
-$clan_name		= $_SERVER['APP_NAME'];											// your gaming clan or organization name
+$clan_name = $_SERVER['APP_NAME'];                                            // your gaming clan or organization name
 
 // PAGE BANNER
-$banner_image	= './common/images/bf4-logo.png';					// your desired page banner image
+$banner_image = './common/images/bf4-logo.png';                    // your desired page banner image
 
 // BANNER LINK
-$banner_url		= $_SERVER['APP_URL'];		// where clicking on page banner will take you
+$banner_url = $_SERVER['APP_URL'];        // where clicking on page banner will take you
